77e85be06ffd336d40f98677d235c71d9e07f036
vivek parihar <vivek@weboniselab.com>
2012-11-28 15:07:37 +0530

    navigation changed and named properly as per there nature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+  def sinatra
+
+  end
+
+  def ruby_on_rails
+
+  end
+
+  def spring_tool_suite
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 EventDocs::Application.routes.draw do
 
-  get "ruby_docs" => "home#ruby_docs"
-  get "sts_docs" => "home#sts_docs"
+  get "sinatra" => "home#sinatra"
+  get "ruby_on_rails" => "home#ruby_on_rails"
+  get "spring_tool_suite" => "home#spring_tool_suite"
 
-  root :to => "home#ruby_docs"
+  root :to => "home#spring_tool_suite"
 end
