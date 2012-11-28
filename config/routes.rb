cba2aff8fc31faed7520edf16cd0b275b06ea236
Apurva Mayank <apurva@weboniselab.com>
2012-11-28 16:15:28 +0530

    rails app doc added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ EventDocs::Application.routes.draw do
 
   get "ruby_docs" => "home#ruby_docs"
   get "sts_docs" => "home#sts_docs"
+  get "ruby_on_rails_docs" => "home#ruby_on_rails_docs"
 
   root :to => "home#ruby_docs"
 end
